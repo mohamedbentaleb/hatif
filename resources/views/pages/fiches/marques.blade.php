718a7241f530430f45d0f46b802bf0a1fe1719cd
Ahmed <ahmed@ahmed.ma>
2022-11-27 18:00:55 +0100

    file-slug

--- resources/views/pages/fiches/marques.blade.php
+++ resources/views/pages/fiches/marques.blade.php
@@ -8,13 +8,24 @@
 
 @section('content')
  
+<div class="row" >
     @foreach ($fiches as $d)
-        <div>
+        
+
+    
+    <div class="col-sm-6" >
+        <div class="card" >
             <a href="{{ route('fiche.modeles' , ['marque' => $d['title'] ] ) }}">
-                <img src="{{ asset('public/logo/'.$d["title"]) }}" alt="{{  $d["title"]; }}" title="{{  $d["title"]; }}">
-                {{  $d["title"]; }}
+                <img   src="{{ asset('logo/'.$d["logo"]) }}" alt="{{  $d["title"]; }}" title="{{  $d["title"]; }}"  width="250px;">
+                <div class="card-body">
+                <h5 class="card-title">{{  $d["title"]; }}</h5>
+                </div>
             </a>
-        </div> 
+        </div>
+    </div>
+
+
     @endforeach
+</div>
    
 @endsection
